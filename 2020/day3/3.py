d5428de49d5379cb1dc5cd250391ed3233eed3b8
crearth <46131286+crearth@users.noreply.github.com>
2020-12-29 13:23:29 +0100

    bijna

--- 2020/day3/3.py
+++ 2020/day3/3.py
@@ -4,7 +4,7 @@ with open('data.txt') as fp:
 #print(d)
 
 d = d.split('\n')
-
+d.pop(0) # delete first row
 #print(d)
 
 y = []
@@ -13,25 +13,28 @@ for n in length:
     y.append(n) 
 #print(y)
 
-x = 0
-d_n = []
+# make array with only odd rows <-- niet nodig
+"""x = 0
+d_o = []
 while x < len(d):
     mod = x % 2
     if mod <= 0:
-        d_n.append(d[x]) 
+        d_o.append(d[x]) 
     x += 1
+"""
 
-xc = 4
-xx = 1 
-while xc < len(d_n):
-    for c in d_n:
-        print(c[xc])
+xc = 3 # start on x coordinate 4
+xx = 3 # second start (every ten times) 
+for r in d:
+    if xc <= 31:
         print(xc)
-        if xc < 31:
-            xc += 3 
-        else:
-            xc = xx
-            xx += 1
+        print(r[xc])
+        xc += 3
+    elif xc > 31:
+        xx -= 1
+        xc = xx
+        print(r[xc])
+        print("xc is %s en xx is %s" % (xc, xx))
 
 """xc = 4
 yc = 0
