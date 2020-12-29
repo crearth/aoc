800cb54e6f6f0da436cb062b90286732a23cda4c
crearth <46131286+crearth@users.noreply.github.com>
2020-12-29 16:45:04 +0100

    something is workinggit add .!

--- 2020/day3/3.py
+++ 2020/day3/3.py
@@ -1,3 +1,7 @@
+from itertools import cycle
+
+
+
 with open('data.txt') as fp:
     d = fp.read()
 
@@ -7,10 +11,11 @@ d = d.split('\n')
 #d.pop(0) # delete first row
 #print(d)
 
-y = []
-length = range(0, len(d))
+y = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+"""length = range(10, len(d))
 for n in length:
     y.append(n) 
+    """
 #print(y)
 
 # make array with only odd rows <-- niet nodig
@@ -36,7 +41,75 @@ xx = 3 # second start (every ten times)
         print("xc is %s en xx is %s" % (xc, xx))
         """
 
-z = 1
+v = 1
+v_new = 10
+h = 3
+h_l = [2, 1, 0]
+while v <= 10: 
+    print(v)
+    print(d[v][h])
+    v += 1
+    h += 3
+for hpos in cycle(h_l):
+    h = hpos
+    for vpos in y:
+        while (h <= 30): 
+            print("v is %s, h is %s" % (v, h))
+            print(d[v][h])
+            v += 1
+            h += 3
+
+"""
+while v <= 322:
+    while v <= 10: # van 1 tot en met 10
+        print("v is %s, h is %s" % (v, h))
+        print(d[v][h])
+        v += 1
+        h += 3 
+    h = 2
+    while v <= 20: # van 11 tot en met 20
+        print("v is %s, h is %s" % (v, h))
+        print(d[v][h])
+        v += 1
+        h += 3
+    h = 1
+    while v <= 30: # van 21 tot en met 30
+        print("v is %s, h is %s" % (v,h)) 
+        print(d[v][h])
+        v += 1
+        h += 3
+    h = 0
+    while v <= 40: # van 31 tot en met 40
+        print("v is %s, h is %s" % (v, h))
+        print(d[v][h])
+        v += 1
+        h += 3
+    h = 2 
+    while v <= 50: 
+        print("v is %s, h is %s" % (v, h))
+        print(d[v][h])
+        v += 1
+        h += 3
+    h = 1
+    while v <= 60: 
+        print("v is %s, h is %s" % (v,h)) 
+        print(d[v][h])
+        v += 1
+        h += 3
+    h = 0
+    while v <= 70: 
+        print("v is %s, h is %s" % (v, h))
+        print(d[v][h])
+        v += 1
+        h += 3
+ 
+    for position in cycle(reversed(range(len(d[0])))):
+        print("v is %s, h is %s" % (v, position)) 
+        print(d[v][position])
+        v += 1
+        """
+
+"""z = 1
 x = 3
 while z <= 322:
     while x <= 31:
@@ -50,6 +123,7 @@ while z <= 322:
         print(d[z][x])
         x += 3
         z += 1
+        """
 
 
 """xc = 4
