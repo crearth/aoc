f9b50e8192cb6dc73fbad4660eb0878d41a960e8
crearth <46131286+crearth@users.noreply.github.com>
2020-12-29 17:30:14 +0100

    its working!

--- 2020/day3/3.py
+++ 2020/day3/3.py
@@ -11,12 +11,13 @@ d = d.split('\n')
 #d.pop(0) # delete first row
 #print(d)
 
-y = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+y = [10]
 """length = range(10, len(d))
 for n in length:
     y.append(n) 
-    """
 #print(y)
+"""
+
 
 # make array with only odd rows <-- niet nodig
 """x = 0
@@ -40,24 +41,35 @@ xx = 3 # second start (every ten times)
         xc = xx
         print("xc is %s en xx is %s" % (xc, xx))
         """
+count = 0
 
 v = 1
-v_new = 10
 h = 3
 h_l = [2, 1, 0]
 while v <= 10: 
-    print(v)
+    print("v is %s, h is %s" % (v, h))
     print(d[v][h])
+    if d[v][h] == "#":
+        count += 1
+        print("eerste 10 count is: %s" % (count))
     v += 1
     h += 3
-for hpos in cycle(h_l):
-    h = hpos
-    for vpos in y:
-        while (h <= 30): 
-            print("v is %s, h is %s" % (v, h))
-            print(d[v][h])
-            v += 1
-            h += 3
+    print("vbegin is %s" % (v))
+while v <= 32:
+
+    for hpos in cycle(h_l):
+        h = hpos
+        for vpos in y:
+            while (h <= 30): 
+                print("v is %s, h is %s" % (v, h))
+                print(d[v][h])
+                if d[v][h] == "#":
+                    count += 1
+                    print("count is: %s" % (count))
+                v += 1
+                h += 3
+
+print(count)
 
 """
 while v <= 322:
